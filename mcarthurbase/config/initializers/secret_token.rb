83f483afe779ac7a98c50145106c18f54c8d7461
A. Samuel Pottinger <Samnsparky@gmail.com>
2012-01-21 18:05:04 -0700

    Created app

---
+++ mcarthurbase/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mcarthurbase::Application.config.secret_token = 'f7b2eecc7d242ca6b4578caf814868867de91cb8dfd173de2a1c38986b874d05978eb01a824713b07c614411642822a8d6c7f0053e3f6811b88eeed2371c600b'
